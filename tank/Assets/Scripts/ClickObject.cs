e430207e58d8b0fe1d44aa2ade0ae921771babc2
tacthgin <821910196@qq.com>
2017-02-28 14:16:46 +0800

    create unity tank

---
+++ tank/Assets/Scripts/ClickObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickObject : MonoBehaviour {
+    private string _currentSoundBtnState = "normal";
+	// Use this for initialization
+	void Start ()
+    {
+        string[] btnNameList = {
+            "playBtn",
+            "moreBtn",
+            "soundBtn"
+        };
+
+        for (int i = 0; i < btnNameList.Length; ++i)
+        {
+            GameObject gameObject = GameObject.Find(btnNameList[i]);
+            Button button = gameObject.GetComponent<Button>();
+            button.onClick.AddListener(delegate ()
+            {
+                onClick(gameObject);
+            });
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onClick(GameObject gameObject)
+    {
+        if(gameObject.name == "soundBtn")
+        {
+            _currentSoundBtnState = (_currentSoundBtnState == "normal") ? "select" : "normal";
+            string btnName = (_currentSoundBtnState == "normal") ? "sound_normal" : "sound_select";
+            Image btnImage = gameObject.GetComponent<Image>();
+            btnImage.sprite = Resources.Load("Material/Button/" + btnName, typeof(Sprite)) as Sprite;
+            if(_currentSoundBtnState == "normal")
+            {
+                Debug.Log("click normal");
+            }else
+            {
+                Debug.Log("click select");
+            }
+        }
+    }
+}
