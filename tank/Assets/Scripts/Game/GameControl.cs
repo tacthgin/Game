7a1a56cd2b244c4b2545fa33635d2ac8f427063e
tacthgin <821910196@qq.com>
2017-04-12 19:30:58 +0800

    添加摇杠点击方式

--- tank/Assets/Scripts/Game/GameControl.cs
+++ tank/Assets/Scripts/Game/GameControl.cs
@@ -8,10 +8,18 @@ public class GameControl : MonoBehaviour{
     private int _life = 3;
     private int _enemy = 20;
 
+    private GameObject _joysitckClickPanel = null;
+    private GameObject _joystickBg = null;
+    private GameObject _joystickCenter = null;
+    private Vector2 _joystickBeginPos = new Vector2();
+
 	void Start ()
     {
-		
-	}
+        _joysitckClickPanel = GameObject.Find("joystickClickPanel");
+        _joystickBg = GameObject.Find("joystickBg");
+        _joystickBeginPos = ((RectTransform)_joystickBg.transform).anchoredPosition;
+        _joystickCenter = GameObject.Find("joystickCenter");
+    }
 	
 	void Update ()
     {
@@ -20,16 +28,31 @@ public class GameControl : MonoBehaviour{
 
     public void OnJoystickBgDown()
     {
-        Debug.Log("down");
+        setClickPos(_joysitckClickPanel, _joystickBg);
     }
 
     public void OnJoystickDrag()
     {
-        Debug.Log("move");
+        setClickPos(_joystickBg, _joystickCenter);
     }
 
     public void OnJoystickBgUp()
     {
-        Debug.Log("up");
+        ((RectTransform)_joystickBg.transform).anchoredPosition = _joystickBeginPos;
+        ((RectTransform)_joystickCenter.transform).anchoredPosition = new Vector2(0, 0);
+    }
+
+    void setClickPos(GameObject parent, GameObject moveObject)
+    {
+        RectTransform parentTransform = (RectTransform)parent.transform;
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentTransform, Input.mousePosition, Camera.main, out pos);
+        RectTransform childTransform = (RectTransform)moveObject.transform;
+        childTransform.anchoredPosition = pos;
+    }
+
+    void getDirection()
+    {
+
     }
 }
