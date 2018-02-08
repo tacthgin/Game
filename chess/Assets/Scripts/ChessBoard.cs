5daed41aa943e9aea9023732feeb56ff8eca4d17
tacthgin <821910196@qq.com>
2018-02-08 16:15:01 +0800

    添加棋盘绘制

---
+++ chess/Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+    private float[] xLocation = { -2.488f, -1.866f, -1.244f, -0.622f, 0.0f, 0.622f, 1.244f, 1.866f, 2.488f };
+    private float[] yLocation = { 2.8f, 2.178f, 1.556f, 0.934f, 0.312f, -0.31f, -0.932f, -1.554f, -2.176f, -2.8f};
+    private float squareDelta = 0.622f;
+    private Dictionary<int, GameObject> pieceDict = new Dictionary<int, GameObject>();
+
+
+    [SerializeField]
+    private GameObject[] pieceGameObject;
+    private ChessLogic chessLogic = new ChessLogic();
+
+    void Start ()
+    {
+        chessLogic.init();
+        drawBoard(chessLogic.CurrentChessBoard);
+	}
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            Debug.Log(hit.point);
+        }
+	}
+
+    void drawBoard(int[] board)
+    {
+        for(int i = ChessLogic.COLUMN_LEFT; i <= ChessLogic.COLUMN_RIGHT; ++i)
+        {
+            for(int j = ChessLogic.ROW_TOP; j <= ChessLogic.ROW_BOTTOM; ++j)
+            {
+                int pc = board[chessLogic.CoordXY(i, j)];
+                if(pc != 0)
+                {
+                    int pieceId = chessLogic.PieceTag(pc);
+                    addPiece(pieceId, i, j);
+                }
+            }
+        }
+    }
+
+    void setPiecePosition(GameObject piece, int x, int y)
+    {
+        piece.transform.position = new Vector2(xLocation[x - ChessLogic.COLUMN_LEFT], yLocation[y - ChessLogic.ROW_TOP]);
+    }
+
+    void addPiece(int pieceId, int x, int y)
+    {
+        GameObject piece = Instantiate(pieceGameObject[pieceId]);
+        piece.transform.parent = transform;
+        setPiecePosition(piece, x, y);
+        pieceDict.Add(chessLogic.CoordXY(x, y), piece);
+    }
+
+    void movePiece(int srcX, int srcY, int dstX, int dstY)
+    {
+        int srcKey = chessLogic.CoordXY(srcX, srcY);
+        if(pieceDict.ContainsKey(srcKey))
+        {
+            GameObject piece = pieceDict[srcKey];
+            pieceDict.Remove(srcKey);
+
+            int dstKey = chessLogic.CoordXY(dstX, dstY);
+            pieceDict[dstKey] = piece;
+            setPiecePosition(piece, dstX, dstY);
+        }
+    }
+
+    void eatPiece(int x, int y)
+    {
+        int key = chessLogic.CoordXY(x, y);
+        if (pieceDict.ContainsKey(key))
+        {
+            GameObject piece = pieceDict[key];
+            Destroy(piece);
+            pieceDict.Remove(key);
+        }
+    }
+}
---
+++ chess/Assets/Scripts/ChessLogic.cs
@@ -0,0 +1,109 @@
+﻿
+public class ChessLogic
+{
+    /// <summary>
+    /// 棋盘范围
+    /// </summary>
+    public const int ROW_TOP = 3;
+    public const int ROW_BOTTOM = 12;
+    public const int COLUMN_LEFT = 3;
+    public const int COLUMN_RIGHT = 11;
+
+    /// <summary>
+    /// 棋子的编号
+    /// </summary>
+    public int PIECE_KING = 0;
+    public int PIECE_ADVISOR = 1;
+    public int PIECE_BISHOP = 2;
+    public int PIECE_KNIGHT = 3;
+    public int PIECE_ROOK = 4;
+    public int PIECE_CANNON = 5;
+    public int PIECE_PAWN = 6;
+
+    /// <summary>
+    /// 初始棋盘
+    /// </summary>
+    public readonly int[] startupChessBoard= {
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0, 21,  0,  0,  0,  0,  0, 21,  0,  0,  0,  0,  0,
+        0,  0,  0, 22,  0, 22,  0, 22,  0, 22,  0, 22,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0, 14,  0, 14,  0, 14,  0, 14,  0, 14,  0,  0,  0,  0,
+        0,  0,  0,  0, 13,  0,  0,  0,  0,  0, 13,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0, 12, 11, 10,  9,  8,  9, 10, 11, 12,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+    };
+
+    /// <summary>
+    /// 测试面板
+    /// </summary>
+    public readonly int[] testChessBoard = {
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+    };
+
+    private int[] currentChessBoard = new int[256];
+
+    public int[] CurrentChessBoard
+    {
+        set { }
+        get { return currentChessBoard; }
+    }
+
+    public void init()
+    {
+        startupChessBoard.CopyTo(currentChessBoard, 0);
+    }
+
+    /// <summary>
+    /// 根据横坐标，纵坐标获得格子
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public int CoordXY(int x, int y)
+    {
+        return x + (y << 4);
+    }
+
+    /// <summary>
+    /// 获得红黑标记(0是红，1是黑)
+    /// </summary>
+    /// <param name="sd"></param>
+    public int SideTag(int sd)
+    {
+        return 8 + (sd << 3);
+    }
+
+    /// <summary>
+    /// 根据棋盘上的值，生成对应的棋子编号 
+    /// </summary>
+    /// <param name="pe"></param>
+    /// <returns></returns>
+    public int PieceTag(int pc)
+    {
+        return pc - 8 - (pc >= 16 ? 1 : 0);
+    }
+}
