50e7daa912ec15a12a38e48d0f5fb1af82138d8f
tacthgin <821910196@qq.com>
2018-03-07 12:17:13 +0800

    添加历史走法信息

---
+++ Chess/Assets/Scripts/MoveInfo.cs
@@ -0,0 +1,33 @@
+﻿
+//历史走法信息
+public class MoveInfo 
+{
+    private ushort mv = 0;
+    private byte pcCaptured = 0;
+    private byte check = 0;
+    private uint key = 0;
+
+    public ushort Mv
+    {
+        set { mv = value; }
+        get { return mv; }
+    }
+
+    public byte PcCaptured
+    {
+        set { pcCaptured = value; }
+        get { return pcCaptured; }
+    }
+
+    public byte Check
+    {
+        set { check = value; }
+        get { return check; }
+    }
+
+    public uint Key
+    {
+        set { key = value; }
+        get { return key; }
+    }
+}
